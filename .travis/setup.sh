403b933e69858b9b492baee82ac5802a61546bc3
Kevin Kuo <kevinstatusk@gmail.com>
2017-05-11 23:59:22 -0700

    test10

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -2,8 +2,8 @@
 
 if $TRAVIS_BRANCH == 'master'; then
   echo "setting production"
-  export NODE_ENV=production
+  export NODE_ENV="production"
 else
   echo "setting staging"
-  export NODE_ENV=staging
+  export NODE_ENV="staging"
 fi
